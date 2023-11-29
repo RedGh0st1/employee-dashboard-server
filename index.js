2ab1c3223e59bf700f2d015bfded6b0d2b87b374
Lennie Nurse <lnursej@gmail.com>
2023-11-29 12:19:32 -0500

    second commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express")
+const app = express()
+const data = require("./data/employeesData.json")
+
+const port = process.env.PORT || 8000
+
+app.get("/employee", (request, response) => {
+  response.send({ employees: data.employees })
+})
+
+app.listen(port, () => {
+  console.log(`Employee Dashboard server running on port: ${port}`)
+})
