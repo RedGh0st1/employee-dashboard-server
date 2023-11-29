1291a962180f9b6e6afb4539c57dd83b1c8d442e
Lennie Nurse <lnursej@gmail.com>
2023-11-29 14:35:04 -0500

    fourth commit "post router"

--- index.js
+++ index.js
@@ -10,6 +10,22 @@ app.get("/employee", (request, response) => {
   response.send({ employees: data.employees })
 })
 
+app.get("/employee/:id", (request, response) => {
+  //   const { id } = req.params.id
+  const employeeId = data.employees.find(
+    (employee) => employee.id === req.params.id
+  )
+  if (employeeId) {
+    return response.send({ employee: employeeId })
+  } else {
+    response.status(404).send({ employee: null })
+  }
+})
+
+app.post("/", (request, response) => {
+  response.send({ message: "Employee added" })
+})
+
 app.listen(port, () => {
   console.log(`Employee Dashboard server running on port: ${port}`)
 })
