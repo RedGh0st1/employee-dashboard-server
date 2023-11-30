354c5a7a30c4b292cecd3fab1b1e4813570e7e44
Lennie Nurse <lnursej@gmail.com>
2023-11-30 11:43:39 -0500

    add body-parser

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require("express")
 const app = express()
 const data = require("./data/employeesData.json")
 const cors = require("cors")
+const bodyParser = require("body-parser")
 
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
 const port = process.env.PORT || 8000
 
@@ -13,7 +15,7 @@ app.get("/employee", (request, response) => {
 app.get("/employee/:id", (request, response) => {
   //   const { id } = req.params.id
   const employeeId = data.employees.find(
-    (employee) => employee.id === req.params.id
+    (employee) => employee.id === request.params.id
   )
   if (employeeId) {
     return response.send({ employee: employeeId })
@@ -22,8 +24,10 @@ app.get("/employee/:id", (request, response) => {
   }
 })
 
-app.post("/", (request, response) => {
-  response.send({ message: "Employee added" })
+app.post("/employee", (request, response) => {
+  // will receive data from client
+  console.log("POST RECEIVED", req.body)
+  //   response.send({ message: "Employee added" })
 })
 
 app.listen(port, () => {
